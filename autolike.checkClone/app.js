51dbcdb3d269408c3855d5180133e7b1465d0ee5
nguyenduclai1998 <kenchivas1998@gmail.com>
2020-09-03 11:14:50 +0700

    Update cron

--- autolike.checkClone/app.js
+++ autolike.checkClone/app.js
@@ -44,8 +44,8 @@ const pushDataServiceLogs = async() => {
 					$exists: false
 				},
 				closedTime: {
-					$gte:1597708800000,
-					$lt:1597968000000
+					$gte:1598806800000,
+					$lt:1598893200000
 				}
 				
 			}).limit(5000).toArray()
