e98f19ae8d9c9670ccd56c15670f173792a01b39
nguyenduclai1998 <kenchivas1998@gmail.com>
2020-09-04 14:08:14 +0700

    Update cron

--- totalWanrranty.autofarmer3108/app.js
+++ totalWanrranty.autofarmer3108/app.js
@@ -17,41 +17,88 @@ client.on('error', (err) => {
 })
 
 mongoose.connect('mongodb://134.122.71.253:27017/autolike', { useNewUrlParser: true, useUnifiedTopology: true })
-	.then(() => {
+	.then(async() => {
 		console.log("Connect success");
+		await totalWanrranty();
+		console.log("done")
 	}) 
 	.catch((error) => {
 		console.log("connect error" + error)
 	})
 cron.schedule('*/5 * * * *', async() => {
-	const tokenDailyStat = await db.collection("daily_stat").distinct("token", {
-		closedTime: {
-			$gte:1598806800000,
-			$lt:1598893200000
-		}
-	})
-	for(const token of tokenDailyStat) {
-		const serviceCodeDailyStat = await db.collection("daily_stat").distinct("service_code", {status:"Closed", token:token})
-		for(const serviceCode of serviceCodeDailyStat) {
-			const dailyStat = await db.collection("daily_stat").findOne({token:token, service_code:serviceCode})
-			const totalWanrranty = await db.collection("service_logs").find({
-			    service_code: serviceCode,
-			    token: token,
-			    $or: [{
-			        checkpoint: true
-			    }, {
-			        hasavatar: false
-			    }]
-			}).count()
-			const updateDaily = {
-				totalWanrranty: totalWanrranty,
-				amount: (parseInt(dailyStat.price) * parseInt(dailyStat.total)) - (parseInt(totalWanrranty) * parseInt(dailyStat.price)),
-				warrantyCosts: parseInt(totalWanrranty) * parseInt(dailyStat.price)
-			}
-			await db.collection("daily_stat").updateOne({token:token, service_code:serviceCode}, {$set:updateDaily})
-			console.log("token:" + token + ",service_code" + serviceCode + "tong bao hanh:" +totalWanrranty )
-			console.log(updateDaily)
-		}
-	}
+	
 })
-const waitFor = (ms) => new Promise(r => setTimeout(r, ms))
+
+const totalWanrranty = async() => {
+	const totalWanrranty = await db.collection("service_logs").find({
+		closedTime: {
+			$gte:1599066000000,
+			$lt:1599152400000
+		},
+	    $or: [{
+	        checkpoint: true
+	    }, {
+	        hasavatar: false
+	    }]
+	}).toArray()
+
+	let mapServiceLog = {}
+	totalWanrranty.forEach( value => {
+		if( !mapServiceLog[ value.service_code + "-" + value.token ] ) {
+			mapServiceLog[ value.service_code + "-" + value.token ] = {}
+		 	mapServiceLog[ value.service_code + "-" + value.token ]['totalLog'] = 1		
+		 	mapServiceLog[ value.service_code + "-" + value.token ]['price'] = value.price
+		 	mapServiceLog[ value.service_code + "-" + value.token ]['data'] = []
+		 	mapServiceLog[ value.service_code + "-" + value.token ]['data'].push(value)
+		 	mapServiceLog[ value.service_code + "-" + value.token ]['totalPrice'] = 0
+		 	mapServiceLog[ value.service_code + "-" + value.token ]['token'] = value.token
+		 	mapServiceLog[ value.service_code + "-" + value.token ]['service_code'] = value.service_code
+		 	mapServiceLog[ value.service_code + "-" + value.token ]['type'] = value.type
+		} else {
+			mapServiceLog[ value.service_code + "-" + value.token ]['totalLog']++
+		}
+		mapServiceLog[ value.service_code + "-" + value.token ]['totalPrice'] = mapServiceLog[ value.service_code + "-" + value.token ]['price'] * mapServiceLog[ value.service_code + "-" + value.token ]['totalLog']
+	});
+	insertDailyStat( Object.values(mapServiceLog)).then(data => {  
+		console.log('xong 1 service')
+	})
+}
+
+function insertDailyStat(listServiceCodeToken) {
+	 return new Promise((resolve, reject) => {
+       	let results = [];
+       	let completed = 0;
+       
+       	listServiceCodeToken.forEach((value, index) => {
+       		let paramUpdate = {
+       			token: value.token,
+				service_code:value.service_code,
+       		}
+
+       		let paramInsert = {
+       			totalWanrranty: value.totalLog,
+       			warrantyCosts:value.totalPrice,
+				updated_at:new Date().valueOf()
+       		}
+
+            Promise.resolve( db.collection("daily_stat").updateOne({
+            	token: value.token,
+				service_code:value.service_code,
+            }, {
+            	$set: paramInsert
+            }))
+            .then(result => {
+                results[index] = result;
+                completed += 1;
+                
+                if (completed == listServiceCodeToken.length) {
+                    resolve(results);
+                }
+            }).catch(err => reject(err));
+            console.log('service_code: ' + value.service_code + ', token: ' + value.token)
+       });
+    });
+}
+
+
+
