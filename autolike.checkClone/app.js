59e3844175bc6a2465e6f0c88a1fd4081b7c8f88
nguyenduclai1998 <kenchivas1998@gmail.com>
2020-09-04 10:37:46 +0700

    update cron

--- autolike.checkClone/app.js
+++ autolike.checkClone/app.js
@@ -17,60 +17,73 @@ client.on('error', (err) => {
 })
 
 mongoose.connect('mongodb://134.122.71.253:27017/autolike', { useNewUrlParser: true, useUnifiedTopology: true })
-	.then(() => {
+	.then(async() => {
 		console.log("Connect success");
+		
+
 	}) 
 	.catch((error) => {
 		console.log("connect error")
 	})
-cron.schedule('*/1 * * * *', async() => {
+cron.schedule('*/30 * * * * *', async() => {
 	await pushDataServiceLogs();
 })
 
-cron.schedule('*/1 * * * *', async() => {
+cron.schedule('*/5 * * * * *', async() => {
 	await popDataServiceLogs();
 })
 
 const pushDataServiceLogs = async() => {
-	client.llen("service_logs", async function(err, reply) {
+	client.llen("check_clone", async function(err, reply) {
 		console.log("So phan tu trong queue: "+reply)
 		if(reply > 95000) {
 			console.log("Queue Service_logs da du");
 			console.log("So phan tu trong queue: "+reply)
 		} else {
 
-			const dataServiceLogs = await db.collection("service_logs").find({
-				checked: {
-					$exists: false
-				},
-				closedTime: {
-					$gte:1598806800000,
-					$lt:1598893200000
+			const dataServiceLogs = await db.collection("service_logs").distinct("uid",{
+			    createdAt: {
+			        $gte:1598461200000,
+			        $lt: 1598547600000
+			    },
+			    checked: {
+			        $exists: false
+			    }
+			})
+			if(dataServiceLogs == 0) {
+				console.log('Het data')
+			} else {
+				for(const item of dataServiceLogs) {
+					await waitFor(50);
+					await db.collection("service_logs").updateMany({
+						uid:item,
+						createdAt: {
+					        $gte:1598461200000,
+					        $lt: 1598547600000
+					    }}, {
+					    	$set:{
+					    		checked: true
+					    	}
+					    })
+					client.rpush("check_clone", JSON.stringify(item), function (err, reply){
+
+					});
 				}
-				
-			}).limit(5000).toArray()
-			for(const item of dataServiceLogs) {
-				await waitFor(50);
-				await db.collection("service_logs").updateOne({_id:mongoose.Types.ObjectId(item._id)}, {$set:{checked: true}})
-				client.rpush("service_logs", JSON.stringify(item), function (err, reply){
-
-				});
-
 			}
 		}
 	})
 }
 
 const popDataServiceLogs = async() => {
-	for(var i = 1; i <= 5000; i++) {
-		client.lpop("service_logs", async function(err, reply) {
+	for(var i = 1; i <= 500; i++) {
+		client.lpop("check_clone", async function(err, reply) {
 		
 		if(reply == null || typeof(reply) === "undefined") {
 			console.log("Queue rong")
 		} else {
-			const dataServiceLogs = JSON.parse(reply)
+			const uid = JSON.parse(reply)
 			const optionId = {
-				uri: "http://scorpion.esrax.com/?method=CheckProfiles&object=Api.Facebook&ids="+dataServiceLogs.uid,
+				uri: "http://scorpion.esrax.com/?method=CheckProfiles&object=Api.Facebook&ids="+uid,
 				method: "GET",
 				json: true,
 				body: {
@@ -90,10 +103,18 @@ const popDataServiceLogs = async() => {
 					} else if(item.HasAvatar === false) {
 						updateStatus.hasavatar = false
 					}
-					await db.collection("service_logs").updateMany({_id:mongoose.Types.ObjectId(dataServiceLogs._id)}, {$set: updateStatus})
+					await db.collection("service_logs").updateMany({
+						uid:uid,
+						createdAt: {
+					        $gte:1598461200000,
+					        $lt: 1598547600000
+					    }
+					}, {
+						$set: updateStatus
+					})
 					console.log("Update Success")
 					console.log(updateStatus)
-					console.log("_id:"+dataServiceLogs._id)
+					console.log("_id:"+uid)
 					console.log("id facebook:"+item.Id)
 				}
 			}
