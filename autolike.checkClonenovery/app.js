ebdd8abd2ad82a107fc03c078495c2e0f01e8e0a
nguyenduclai1998 <kenchivas1998@gmail.com>
2020-09-08 17:50:08 +0700

    Update checkpoint

--- autolike.checkClonenovery/app.js
+++ autolike.checkClonenovery/app.js
@@ -95,7 +95,7 @@ const popDataServiceLogs = async() => {
 						})
 						tongCheckpoint++
 					}else {
-
+						console.log('clone van con song =)))')
 					}
 					
 					console.log("Update Success")
