8c31466bf7b2b4c7db543e33d551c67a772071cf
nguyenduclai1998 <kenchivas1998@gmail.com>
2020-09-10 09:42:45 +0700

    Update cron

--- autolike.checkClone/app.js
+++ autolike.checkClone/app.js
@@ -25,7 +25,7 @@ mongoose.connect('mongodb://134.122.71.253:27017/autolike', { useNewUrlParser: t
 	.catch((error) => {
 		console.log("connect error")
 	})
-cron.schedule('10 1 * * *', async() => {
+cron.schedule('45 09 * * *', async() => {
 	await pushDataServiceLogs();
 })
 
