2e8bc84f95d2d5c10564fc1cc48a205036d37a8c
nguyenduclai1998 <kenchivas1998@gmail.com>
2020-09-19 11:42:07 +0700

    Update cron

--- totalWanrranty.autofarmer/app.js
+++ totalWanrranty.autofarmer/app.js
@@ -19,13 +19,13 @@ client.on('error', (err) => {
 mongoose.connect('mongodb://134.122.71.253:27017/autolike', { useNewUrlParser: true, useUnifiedTopology: true })
 	.then(async() => {
 		console.log("Connect success");
-		await wanrranty()
+		// await wanrranty()
 	}) 
 	.catch((error) => {
 		console.log("connect error" + error)
 	})
 cron.schedule('*/59 * * * *', async() => {
-	// await wanrranty()
+	await wanrranty()
 })
 
 var start = new Date();
