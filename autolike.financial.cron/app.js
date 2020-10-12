22d22713d3c41220b6f1ae7df9d4b9c8f6eb63ee
nguyenduclai1998 <kenchivas1998@gmail.com>
2020-10-12 22:12:52 +0700

    Update cron

--- autolike.financial.cron/app.js
+++ autolike.financial.cron/app.js
@@ -37,9 +37,9 @@ const pushDataService = async() => {
 			$lte: endDay - 345600000
 		}
 	})
-	for(const token of tokenDaily) {
+	// for(const token of tokenDaily) {
 		const dataDaily = await db.collection("daily_stat").find({
-			token:token,
+			token:"RJI2G441ODEWOJFEPI8E4G",
 			status: "Closed",
 			closedTime:{
 				$gte:startDay - 345600000,
@@ -51,12 +51,18 @@ const pushDataService = async() => {
 			console.log("Data rong: " + token)
 		} else {
 			let amounts = 0
+			let amount = 0
+			let warrantyCost = 0
 			for(const amount of dataDaily) {
 				if(!amount.warrantyCosts) {
 					amounts = parseInt(amounts)+ parseInt(amount.amount)
 				} else {
 					amounts = parseInt(amounts) + (parseInt(amount.amount) - parseInt(amount.warrantyCosts))
-				}					
+				}
+
+				amount = parseInt(amount)+ parseInt(amount.amount)
+				warrantyCost = parseInt(warrantyCost)+ parseInt(amount.warrantyCost)
+
 			}
 
 			await db.collection("financial").findOneAndUpdate({
@@ -74,8 +80,11 @@ const pushDataService = async() => {
 					upsert: true
 				}
 			)	
+			console.log(amounts)
+			console.log(amount)
+			console.log(warrantyCost)
 		}
-	}
+	// }
 	
 	console.log("startDay: " + (startDay - 345600000), "endDay: " + (endDay - 345600000))
 	console.log('endTime: ' + new Date(startDay - 345600000));
