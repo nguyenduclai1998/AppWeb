9dc2999b7bf012663142e3e116941ca7cba63df5
nguyenduclai1998 <kenchivas1998@gmail.com>
2020-10-12 13:46:50 +0700

    Update cron

---
+++ autolike.financial.cron/app.js
@@ -0,0 +1,83 @@
+import express from 'express';
+import 'regenerator-runtime/runtime';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import logger from 'morgan';
+import cron from 'node-cron';
+import request from 'request-promise'
+const db = mongoose.connection
+
+mongoose.connect('mongodb://134.122.71.253:27017/autolike', { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(async () => {
+		console.log("Connect success");
+		await pushDataService()
+	}) 
+	.catch((error) => {
+		console.log("connect error"+error)
+	})
+// cron.schedule('*/59 * * * *', async() => {
+// 	await pushDataService()
+// })
+
+var start = new Date();
+start.setHours(0,0,0,0);
+
+var end = new Date();
+end.setHours(23,59,59,999);
+
+var startDay = start.valueOf()
+var endDay = end.valueOf();
+
+const pushDataService = async() => {
+	console.log("------------------Bắt đầu một chu kì------------------")
+	console.log('timeStart: ' + new Date());
+	const tokenDaily = await db.collection("daily_stat").distinct("token", {
+		closedTime:{
+			$gte:startDay - 1296000000,
+			$lt: endDay - 1296000000
+		}
+	})
+	for(const token of tokenDaily) {
+		const dataDaily = await db.collection("daily_stat").find({
+			token:token,
+			status: "Closed",
+			closedTime:{
+				$gte:startDay - 1296000000,
+				$lt: endDay - 1296000000
+			}
+		}).toArray()
+		
+		if(dataDaily.length == 0) {
+			console.log("Data rong: " + token)
+		} else {
+			let amounts = 0
+			for(const amount of dataDaily) {
+				if(!amount.warrantyCosts) {
+					amounts = parseInt(amounts)+ parseInt(amount.amount)
+				} else {
+					amounts = parseInt(amounts) + (parseInt(amount.amount) - parseInt(amount.warrantyCosts))
+				}					
+			}
+
+			await db.collection("financial").findOneAndUpdate({
+				token:token,
+				closedTime: startDay - 1296000000,
+			}, {
+				$set: {
+					closedTimeISO:new Date(startDay - 1296000000).toLocaleDateString(),
+					update_at:new Date().getTime(),
+					amount: amounts,
+					status:"Chưa thanh toán",
+					statusNumber:0
+				}},
+				{
+					upsert: true
+				}
+			)	
+		}
+	}
+	
+	console.log("startDay: " + (startDay - 1296000000), "endDay: " + (endDay - 1296000000))
+	console.log('endTime: ' + new Date());
+	console.log("------------------Kết thúc một chu kì------------------")
+}
---
+++ autolike.financial.cron/readme.md
@@ -0,0 +1,18 @@
+﻿npm install babel-register babel-preset-env --save-dev
+
+Create a starter.js 
+require("babel-register")({
+  presets: ["env"],
+});
+
+// Import the rest of our application.
+module.exports = require("./test.js");
+
+App.js 
+có cái import
+import regeneratorRuntime from "regenerator-runtime";
+
+chạy test
+node starters.js
+
+is_encript
