67db091ba65cf70735a247bf212ad7ae12be3048
nguyenduclai1998 <kenchivas1998@gmail.com>
2020-10-12 21:12:57 +0700

    Update cron

--- totalWanrranty.autofarmer/app.js
+++ totalWanrranty.autofarmer/app.js
@@ -31,8 +31,8 @@ var endDay = end.valueOf();
 const wanrranty = async() => {
 	const serviceSuccess = await db.collection("services").distinct("service_code",{
 		TimeSuccess: {
-	        $gte: startDay - 1123200000,
-	        $lt: endDay - 1123200000
+	        $gte: startDay - 1036800000,
+	        $lt: endDay - 1036800000
 	    },
 	    $or: [{
 	        status: "Success"
