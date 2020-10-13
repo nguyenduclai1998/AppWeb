3492847d66e6d6a36fdabb39bf583ee4d96122e1
nguyenduclai1998 <kenchivas1998@gmail.com>
2020-10-13 13:20:09 +0700

    Update cron

--- autolike.financial.cron/app.js
+++ autolike.financial.cron/app.js
@@ -37,9 +37,9 @@ const pushDataService = async() => {
 			$lte: endDay - 345600000
 		}
 	})
-	// for(const token of tokenDaily) {
+	for(const token of tokenDaily) {
 		const dataDaily = await db.collection("daily_stat").find({
-			token:"RJI2G441ODEWOJFEPI8E4G",
+			token:token,
 			status: "Closed",
 			closedTime:{
 				$gte:startDay - 345600000,
@@ -51,47 +51,33 @@ const pushDataService = async() => {
 			console.log("Data rong: " + token)
 		} else {
 			let amounts = 0
-			let amount1 = 0
-			let warrantyCost = 0
-			let totalWanrranty = 0
-			let total = 0
 			for(const amount of dataDaily) {
 				if(!amount.warrantyCosts) {
 					amounts = parseInt(amounts)+ parseInt(amount.amount)
-					warrantyCost = warrantyCost
-					totalWanrranty = totalWanrranty
+
 				} else {
 					amounts = parseInt(amounts) + (parseInt(amount.amount) - parseInt(amount.warrantyCosts))
-					warrantyCost = parseInt(warrantyCost)+ parseInt(amount.warrantyCosts)
-					totalWanrranty = parseInt(totalWanrranty)+ parseInt(amount.totalWanrranty)
-				}
 
-				amount1 = parseInt(amount1)+ parseInt(amount.amount)
-				total = parseInt(total)+ parseInt(amount.total)
+				}
 			}
 
-			// await db.collection("financial").findOneAndUpdate({
-			// 	token:token,
-			// 	closedTime: startDay - 345600000,
-			// }, {
-			// 	$set: {
-			// 		closedTimeISO:new Date(startDay - 345600000).toLocaleDateString(),
-			// 		update_at:new Date().getTime(),
-			// 		amount: amounts,
-			// 		status:"Chưa thanh toán",
-			// 		statusNumber:0
-			// 	}},
-			// 	{
-			// 		upsert: true
-			// 	}
-			// )	
-			console.log("tien truoc khi tru bao hanh: " + amount1)
-			console.log("tien sau khi tru bao hanh: " + amounts)
-			console.log("tong tien bao hanh: " + warrantyCost)
-			console.log("Tong so likepage + follow: " + total)
-			console.log("Tong so likepage + follow bi checkpoint: " + totalWanrranty)
+			await db.collection("financial").findOneAndUpdate({
+				token:token,
+				closedTime: startDay - 345600000,
+			}, {
+				$set: {
+					closedTimeISO:new Date(startDay - 345600000).toLocaleDateString(),
+					update_at:new Date().getTime(),
+					amount: amounts,
+					status:"Chưa thanh toán",
+					statusNumber:0
+				}},
+				{
+					upsert: true
+				}
+			)	
 		}
-	// }
+	}
 	
 	console.log("startDay: " + (startDay - 345600000), "endDay: " + (endDay - 345600000))
 	console.log('endTime: ' + new Date(startDay - 345600000));
--- totalWarranty.cron/app.js
+++ totalWarranty.cron/app.js
@@ -24,36 +24,13 @@ mongoose.connect('mongodb://134.122.71.253:27017/autolike', { useNewUrlParser: t
 		console.log("connect error")
 	})
 cron.schedule('*/5 * * * * *', async() => {
-	const data = await db.collection("daily_stat").find({
-		closedTime: {
-	        $gte: 1600102800000,
-	        $lt: 1600189199999
-	    },
-	    token:"RJI2G441ODEWOJFEPI8E4G",
-	    status: "Closed"
-	}).toArray()
-	let tong = 0
-	data.forEach(value => {
-		tong = tong + value.total
-	});
-	console.log(tong)
-	// const dataServices = await db.collection("services").find({
-	// 	status:"Success",
-	// 	created_at: {
-	// 		$lte:1597708800000,
-	// 		$lt:1597968000000
-	// 	}
-	// }).toArray()
-	// for(const item of dataServices) {
-	// 	await waitFor(50);
-	// 	const totalWarranty = await db.collection("service_logs").find({$and:[{service_code:item.service_code, checkpoint:true}]}).toArray()
-	// 	console.log(totalWarranty.length)
-	// 	console.log(item.service_code)
-	// 	const total_warranty = {
-	// 		total_warranty: totalWarranty.length
-	// 	}
-	// 	const dataServiceLogs = await db.collection("services").updateMany({service_code:item.service_code}, {$set: total_warranty})
-	// 	console.log(total_warranty)
-	// }
+	
 })
+
+const baohanh = async() => {
+	const data = await db.collection("service_logs").find({
+		service_code:"HNTK8Q",
+		checkpoint : true
+	})
+}
 const waitFor = (ms) => new Promise(r => setTimeout(r, ms))
